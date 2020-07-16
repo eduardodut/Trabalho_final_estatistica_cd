9a8e84c53ac9e1c2c2dd395c81763ab28a73762e
Eduardo Dutra <eduardo_dut@edu.unifor.br>
2020-07-16 10:21:30 -0300

    correção no dataframe resultante das simulações

--- .history/src/Geracao_dados_20200716100645.py
+++ .history/src/Geracao_dados_20200716100645.py
@@ -7,9 +7,9 @@ import pandas as pd
 # d = pd.read_pickle('C:/Users/Eduar/Documents/GitHub/Trabalho_final_estatistica_cd/dados/simulacoes_chance_100%.pkl')
 # d.to_csv(r'C:/Users/Eduar/Documents/GitHub/Trabalho_final_estatistica_cd/dados/simulacoes_chance_100%.txt', sep=' ', index=False)
 # print(d.head())
-nome_simulacao = "nsimulacoes_chance_100%" 
-n_simulacoes = 1
-tamanho_matriz = 6
+nome_simulacao = "simulacoes_chance_100%" 
+n_simulacoes = 5000
+tamanho_matriz = 60
 chance_infeccao = 1 
 chance_infeccao_sintomatico = 0.2
 
@@ -41,45 +41,44 @@ dados_simulacoes = dados_simulacoes[["pop_inicial",
                                      "tipo2_inicial", 
 
                                      "n/2_100%_infectados",
-                                     "tipo1_n/2_100%_infectados",
-                                     "tipo2_n/2_100%_infectados",
-                                     "curados_n/2_100%_infectados",
-                                     "mortos_n/2_100%_infectados",
+                                     "tipo1_n/2",
+                                     "tipo2_n/2",
+                                     "curados_n/2",
+                                     "mortos_n/2",
 
                                      "n/2+1_100%_infectados",
-                                     "tipo1_n/2+1_100%_infectados",
-                                     "tipo2_n/2+1_100%_infectados",
-                                     "curados_n/2+1_100%_infectados",
-                                     "mortos_n/2+1_100%_infectados",
+                                     "tipo1_n/2+1",
+                                     "tipo2_n/2+1",
+                                     "curados_n/2+1",
+                                     "mortos_n/2+1",
 
-                                     "n_100%_infectados",
-                                     "tipo1_n_100%_infectados",
-                                     "tipo2_n_100%_infectados",
-                                     "curados_n_100%_infectados",
-                                     "mortos_n_100%_infectados",
-                                     
-                                     "n/2_pico",
-                                     "tipo1_n/2_pico",
-                                     "tipo2_n/2_pico",
-                                     "curados_n/2_pico",
-                                     "mortos_n/2_pico",
+                                     "n_atualizacoes_100%_infectados",
+                                     "tipo1_n",
+                                     "tipo2_n",
+                                     "curados_n",
+                                     "mortos_n",
+                                     "n/2_atualizacoes_pico",
+                                     "tipo1_n/2pico",
+                                     "tipo2_n/2pico",
+                                     "curados_n/2pico",
+                                     "mortos_n/2pico",
                                     
-                                     "n/2+1_pico",
-                                     "tipo1_n/2+1_pico",
-                                     "tipo2_n/2+1_pico",
-                                     "curados_n/2+1_pico",
-                                     "mortos_n/2+1_pico",
+                                     "n/2+1_atualizacoes_pico",
+                                     "tipo1_n/2+1pico",
+                                     "tipo2_n/2+1pico",
+                                     "curados_n/2+1pico",
+                                     "mortos_n/2+1pico",
 
-                                     "n_pico",
-                                     "tipo1_n_pico",
-                                     "tipo2_n_pico",
-                                     "curados_n_pico",
-                                     "mortos_n_pico",
+                                     "n_atualizacoes_pico",
+                                     "tipo1_pico",
+                                     "tipo2_pico",
+                                     "curados_pico",
+                                     "mortos_pico",
                                      
-                                     "n_total",
-                                     "sadios_n_total",
-                                     "curados_n_total",
-                                     "mortos_n_total"]].astype(int)
+                                     "numero_total_atualizacoes",
+                                     "sadios_final",
+                                     "curados_final",
+                                     "mortos_final"]].astype(int)
 
 
 
@@ -135,17 +134,17 @@ dados_simulacoes = dados_simulacoes[["pop_inicial",
                                      "curados_n/2_pico",
                                      "mortos_n/2_pico",
                                     
-                                     "n/2+1_pico",
+                                     "n/2+1_atualizacoes_pico",
                                      "tipo1_n/2+1_pico",
                                      "tipo2_n/2+1_pico",
                                      "curados_n/2+1_pico",
                                      "mortos_n/2+1_pico",
 
                                      "n_pico",
-                                     "tipo1_n_pico",
-                                     "tipo2_n_pico",
-                                     "curados_n_pico",
-                                     "mortos_n_pico",
+                                     "tipo1_pico",
+                                     "tipo2_pico",
+                                     "curados_pico",
+                                     "mortos_pico",
                                      
                                      "n_total",
                                      "sadios_n_total",
